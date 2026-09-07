ef4a67c8d1018972cff9fa53206754cd677c70cf
s-508 <3e.85s0c@gmail.com>
2026-09-07 12:44:34 -0700

    Fixed recipes only being added to the left input shard's file

--- format-fusions.py
+++ format-fusions.py
@@ -67,6 +67,7 @@ for input_combo, results in data['recipes'].items():
         }
 
         input_recipes[inputs[0]].append(recipe)
+        input_recipes[inputs[1]].append(recipe)
         output_recipes[result_id].append(recipe)
 
 # TODO: sort dictionaries
@@ -76,7 +77,9 @@ def map_input_recipe(recipe):
     return {
         "fusion_type": recipe['fusion_type'],
 
-        # left shard data is implied
+        "left_name": recipe['left_name'],
+        "left_count": recipe['left_count'],
+        "left_skyblock_id": recipe['left_skyblock_id'],
 
         "right_name": recipe['right_name'],
         "right_count": recipe['right_count'],
@@ -110,8 +113,8 @@ for shard_id in input_recipes.keys():
 
     out = {
         "name": shard_name,
-        "fusion_required": shard_data['fuse_amount'],
-        "skyblock_id": shard_data['internal_id'],
+        # "fusion_required": shard_data['fuse_amount'],
+        # "skyblock_id": shard_data['internal_id'],
         
         "input_recipes": list(map(map_input_recipe, input_recipes[shard_id])),
         "output_recipes": list(map(map_output_recipe, output_recipes[shard_id])),
